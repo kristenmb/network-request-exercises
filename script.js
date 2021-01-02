4bbcaa1199f68db0c93a2e56d845af7baa832e7c
Kristen Bair <kristenmbair@gmail.com>
2021-01-01 21:00:20 -0700

    Reorganize script file layout

--- script.js
+++ script.js
@@ -2,7 +2,33 @@ const getButton = document.querySelector("#get");
 const addButton = document.querySelector("#post");
 const deleteButton = document.querySelector("#delete");
 
-getButton.addEventListener('click', async() => {
+// const animals = fetch("http://localhost:3001/api/v1/animals")
+//   .then(response => response.json())
+//   .then (animals => animals)        
+
+// getButton.addEventListener('click', async() => {
+//   const dataDisplay = document.querySelector("#data-display");
+//   const animalList = await animals;
+//   const tempInfo = document.querySelector("#temp");
+//   tempInfo.classList.add('hidden');
+//   animalList.forEach(animal => {
+//     dataDisplay.innerHTML += `
+//     <h2>${animal.id}. ${animal.name}</h2>
+//     <p>Diet: ${animal.diet}</p>
+//     <p>Fun Fact: ${animal.fun_fact}</p>
+//   `
+//   });
+// })
+
+getButton.addEventListener('click', listAnimals);
+addButton.addEventListener('click', addNewAnimal);
+deleteButton.addEventListener('click', removePost);
+
+async function listAnimals() {
+  const animals = fetch("http://localhost:3001/api/v1/animals")
+  .then(response => response.json())
+  .then (animals => animals); 
+
   const dataDisplay = document.querySelector("#data-display");
   const animalList = await animals;
   const tempInfo = document.querySelector("#temp");
@@ -12,30 +38,27 @@ getButton.addEventListener('click', async() => {
     <h2>${animal.id}. ${animal.name}</h2>
     <p>Diet: ${animal.diet}</p>
     <p>Fun Fact: ${animal.fun_fact}</p>
-  `
-  });
-})
-
-addButton.addEventListener('click', addNewAnimal);
-deleteButton.addEventListener('click', removePost);
-
-const animals = fetch("http://localhost:3001/api/v1/animals")
-  .then(response => response.json())
-  .then (animals => animals)        
+  `})
+} 
 
 function addNewAnimal() {
   let nameInput = document.querySelector("#name");
   let dietInput = document.querySelector("#diet");
   let factInput = document.querySelector("#fact");
   let idInput = document.querySelector("#idNum");
+  
   let newAnimal = `{
     "id": "${idInput.value}", 
     "name": "${nameInput.value}", 
     "diet": "${dietInput.value}", 
     "fun_fact": "${factInput.value}"
   }`
+  
   postAnimal(newAnimal);
-  clearInput(idInput, nameInput, dietInput, factInput);
+  clearInput(idInput);
+  clearInput(nameInput);
+  clearInput(dietInput);
+  clearInput(factInput);
 }
 
 function postAnimal(newPost) {
@@ -48,11 +71,8 @@ function postAnimal(newPost) {
       .then(animal => animal)
 }
 
-function clearInput(first, second, third, fourth) {
-  first.value = "";
-  second.value = "";
-  third.value = "";
-  fourth.value = "";
+function clearInput(valueName) {
+  valueName.value = "";
 }
 
 function removePost() {
